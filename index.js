36cd23de88cd53781809695535ae292567c3a830
AnthonySMyers <33797691+AnthonySMyers@users.noreply.github.com>
2018-09-20 16:24:12 -0700

    All Questions from PHQ-9 Added
    Good Vibes now has the complete Patient Depression Questionnaire for a more accurate depression screening test.

--- index.js
+++ index.js
@@ -31,9 +31,15 @@ const languageStrings = {
                 "Don't let your problems get the best of you, there is still so much to live for.",
             ],
             QUESTIONS: [
-                "How often did you feel little interest or pleasure in doing things",
-                "How often did you feel down, depressed or hopeless",
-                'How often did you have trouble falling or staying asleep, or sleeping too much',
+                "How often did you feel little interest or pleasure in doing things?",
+                "How often did you feel down, depressed or hopeless?",
+                'How often did you have trouble falling or staying asleep, or sleeping too much?',
+                "How often did you feel tired or had little energy?",
+                "How often did you have a poor appetite or experience overeating?",
+                "How often did you feel bad about yourself or feel that you are a failure or feel like you let yourself or others down?",
+                "How often did you struggle to concentrate on things, such as reading a book or watching television?",
+                "How often did you move or speak slower or quicker than usual such that other people noticed?",
+                "How ofthen did you have thoughts that you would be better off dead, or of hurting yourself?",
             ],
             IFTHISISBAD:[
                 "Be kind to yourself",
