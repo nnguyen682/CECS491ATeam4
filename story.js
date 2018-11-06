35d5d21470cbbca4e069c6b513df0121d11479a0
jespinoza17 <33881931+jespinoza17@users.noreply.github.com>
2018-11-06 11:15:26 -0800

    Create story.js
    I added the file that will store all the available stories for the user to hear.

---
+++ story.js
@@ -0,0 +1,8 @@
+module.exports =  [
+  "This is a personal story from Nhan. I lost my wallet.",
+  "This is a personal story from Nhan. I'm single.",
+  "This is a personal story from Anthony. Nhan said I am annoying.",
+  "This is a personal story from Anothony. My parents are homeless and I'm trying to finish college so i can support them.",
+  "This is a personal story from Juan. I don't have a sad story",
+  "This is a personal story from Juan. This is Nhan saying on Juan behalf, He doesn't really have any sad story."
+]
